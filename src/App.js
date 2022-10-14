b85c4ad0a0b3b220f5ae0a564a4e2d630228cffa
Dung Jessica Simi <33448970+Priceless10@users.noreply.github.com>
2022-10-14 12:37:04 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,23 +25,23 @@ const handleOldValue = () => {
       <input class="value" type="text" value={input}/>
       <button onClick={handleReset} class="numclear">C</button>
       <button onClick={handleOldValue} class="numclear">Del</button>
-      <button name="*" onClick={(e) => setInput(input.concat(e.target.name))} class="num">*</button>
-      <button name="/" onClick={(e) => setInput(input.concat(e.target.name))} class="num">/</button>
-      <button name="9" onClick={(e) => setInput(input.concat(e.target.name))} class="num">9</button>
-      <button name="8" onClick={(e) => setInput(input.concat(e.target.name))} class="num">8</button>
-      <button name="7" onClick={(e) => setInput(input.concat(e.target.name))} class="num">7</button>
-      <button name="-" onClick={(e) => setInput(input.concat(e.target.name))} class="num sub">-</button>
-      <button name="6" onClick={(e) => setInput(input.concat(e.target.name))} class="num">6</button>
-      <button name="5" onClick={(e) => setInput(input.concat(e.target.name))} class="num">5</button>
-      <button name="4" onClick={(e) => setInput(input.concat(e.target.name))} class="num">4</button>
-      <button name="+" onClick={(e) => setInput(input.concat(e.target.name))} class="num plus">+</button>
-      <button name="3" onClick={(e) => setInput(input.concat(e.target.name))} class="num">3</button>
-      <button name="2" onClick={(e) => setInput(input.concat(e.target.name))} class="num">2</button>
-      <button name="1" onClick={(e) => setInput(input.concat(e.target.name))} class="num">1</button>
-      <button name="%" onClick={(e) => setInput(input.concat(e.target.name))} class="num">%</button>
-      <button name="0" onClick={(e) => setInput(input.concat(e.target.name))} class="num">0</button>
-      <button name="00" onClick={(e) => setInput(input.concat(e.target.name))} class="num">00</button>
-      <button name="000" onClick={(e) => setInput(input.concat(e.target.name))} class="num">000</button>
+      <button name="*" onClick={(e) => setInput(input + e.target.name)} class="num">*</button>
+      <button name="/" onClick={(e) => setInput(input + e.target.name)} class="num">/</button>
+      <button name="9" onClick={(e) => setInput(input + e.target.name)} class="num">9</button>
+      <button name="8" onClick={(e) => setInput(input + e.target.name)} class="num">8</button>
+      <button name="7" onClick={(e) => setInput(input + e.target.name)} class="num">7</button>
+      <button name="-" onClick={(e) => setInput(input + e.target.name)} class="num sub">-</button>
+      <button name="6" onClick={(e) => setInput(input + e.target.name)} class="num">6</button>
+      <button name="5" onClick={(e) => setInput(input + e.target.name)} class="num">5</button>
+      <button name="4" onClick={(e) => setInput(input + e.target.name)} class="num">4</button>
+      <button name="+" onClick={(e) => setInput(input + e.target.name)} class="num plus">+</button>
+      <button name="3" onClick={(e) => setInput(input + e.target.name)} class="num">3</button>
+      <button name="2" onClick={(e) => setInput(input + e.target.name)} class="num">2</button>
+      <button name="1" onClick={(e) => setInput(input + e.target.name)} class="num">1</button>
+      <button name="%" onClick={(e) => setInput(input + e.target.name)} class="num">%</button>
+      <button name="0" onClick={(e) => setInput(input + e.target.name)} class="num">0</button>
+      <button name="00" onClick={(e) => setInput(input + e.target.name)} class="num">00</button>
+      <button name="000" onClick={(e) => setInput(input + e.target.name)} class="num">000</button>
       <button  onClick={handleAnswer} class="num equal">=</button>
 
     </div>
